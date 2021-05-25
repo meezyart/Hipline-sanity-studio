e13c11185b1c7b87509f9ef048e556845570030c
Gamel James <g_james@apple.com>
2021-05-25 02:38:44 -0700

    Update To Repo

--- parts/dashboardConfig.js
+++ parts/dashboardConfig.js
@@ -7,7 +7,7 @@ export default {
             options: {
                 data: [{
                         title: 'Sanity Repo',
-                        value: 'https://github.com/meezyart/meezyart-sanity-starter-cms',
+                        value: 'https://github.com/meezyart/Hipline-sanity-studio',
                         category: 'Code'
                     },
                     {
@@ -24,16 +24,16 @@ export default {
                 title: 'Netlify Deploys',
                 description: 'NOTE: Because these sites are static builds, they need to be re-deployed to see the changes when documents are published.',
                 sites: [{
-                        title: 'Sanity',
-                        apiId: 'XXX',
-                        buildHookId: 'XXX',
-                        name: 'XXX'
+                        buildHookId: '6041599a1dd2e419a608eae5',
+                        title: 'Sanity Studio',
+                        name: 'hipline-sanity-11-ty-studio',
+                        apiId: 'e6e9e62f-f363-4bba-a984-5b9ef6145507'
                     },
                     {
-                        title: 'Website',
-                        apiId: 'XXX',
-                        buildHookId: 'XXX',
-                        name: 'XXX'
+                        buildHookId: '6041599a5936a717e2a5c3ac',
+                        title: 'Blog Website',
+                        name: 'hipline-sanity-11-ty',
+                        apiId: '9eacabf1-2d57-45b0-b4f7-34e6bf6f9f7c'
                     }
                 ]
             }
