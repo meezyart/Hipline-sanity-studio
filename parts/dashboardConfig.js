59739404e34b3f9b91a4da887f9339276a72d970
Gamel James <g_james@apple.com>
2021-05-25 12:07:44 -0700

    update Toml

--- parts/dashboardConfig.js
+++ parts/dashboardConfig.js
@@ -26,7 +26,7 @@ export default {
                 sites: [{
                         buildHookId: '6041599a1dd2e419a608eae5',
                         title: 'Sanity Studio',
-                        name: 'hipline-sanity-11-ty-studio',
+                        name: 'hipline-sanity-studio',
                         apiId: 'e6e9e62f-f363-4bba-a984-5b9ef6145507'
                     },
                     {
@@ -38,8 +38,32 @@ export default {
                 ]
             }
         },
-        {
-            name: 'project-users'
-        }
+        // { name: 'structure-menu' },
+        // {
+        //     name: 'project-info',
+        //     options: {
+        //         __experimental_before: [{
+        //             name: 'netlify',
+        //             options: {
+        //                 description: 'NOTE: Because these sites are static builds, they need to be re-deployed to see the changes when documents are published.',
+        //                 sites: [{
+        //                         buildHookId: '6041599a1dd2e419a608eae5',
+        //                         title: 'Sanity Studio',
+        //                         name: 'hipline-sanity-11-ty-studio',
+        //                         apiId: 'e6e9e62f-f363-4bba-a984-5b9ef6145507'
+        //                     },
+        //                     {
+        //                         buildHookId: '6041599a5936a717e2a5c3ac',
+        //                         title: 'Blog Website',
+        //                         name: 'hipline-sanity-11-ty',
+        //                         apiId: '9eacabf1-2d57-45b0-b4f7-34e6bf6f9f7c'
+        //                     }
+        //                 ]
+        //             }
+        //         }],
+
+        //     }
+        // },
+        { name: 'project-users', layout: { height: 'auto' } }
     ]
 }
