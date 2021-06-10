ea0343048de5297339ecc6aa2b4b3d8bb7b4f0ef
Gamel James <g_james@apple.com>
2021-06-10 16:41:46 -0700

    Added Bullets and links to excert

--- schemas/objects/blocks/excerptPortableText.js
+++ schemas/objects/blocks/excerptPortableText.js
@@ -1,21 +1,82 @@
 export default {
-  name: 'excerptPortableText',
-  type: 'array',
-  title: 'Excerpt',
-  of: [
-    {
-      title: 'Block',
-      type: 'block',
-      styles: [{ title: 'Normal', value: 'normal' }],
-      lists: [],
-      marks: {
-        decorators: [
-          { title: 'Strong', value: 'strong' },
-          { title: 'Emphasis', value: 'em' }
-          // {title: 'Code', value: 'code'}
+    name: 'excerptPortableText',
+    type: 'array',
+    title: 'Excerpt',
+    of: [{
+        title: 'Block',
+        type: 'block',
+        styles: [{
+                title: 'Normal',
+                value: 'normal'
+            },
+            {
+                title: 'H1',
+                value: 'h1'
+            },
+            {
+                title: 'H2',
+                value: 'h2'
+            },
+            {
+                title: 'H3',
+                value: 'h3'
+            },
+            {
+                title: 'H4',
+                value: 'h4'
+            }
         ],
-        annotations: []
-      }
-    }
-  ]
+        lists: [
+            { title: 'Bullet', value: 'bullet' },
+            { title: 'Number', value: 'number' }
+        ],
+        marks: {
+            decorators: [
+                { title: 'Strong', value: 'strong' },
+                { title: 'Emphasis', value: 'em' }
+                // {title: 'Code', value: 'code'}
+            ],
+            annotations: [{
+                    name: 'internalLink',
+                    type: 'object',
+                    title: 'Internal link',
+                    blockEditor: {
+                        icon: FiLink
+                    },
+                    fields: [{
+                            name: 'reference',
+                            type: 'reference',
+                            title: '(A) Internal Page',
+                            to: [{ type: 'article' }, { type: 'page' }]
+                        },
+                        {
+                            title: 'Style as Button?',
+                            name: 'isButton',
+                            type: 'boolean'
+                        }
+                    ]
+                },
+                {
+                    name: 'link',
+                    type: 'object',
+                    title: '(B) External URL',
+                    blockEditor: {
+                        icon: FiExternalLink
+                    },
+                    fields: [{
+                            name: 'href',
+                            type: 'url',
+                            title: 'URL',
+                            description: 'This link will open in a new tab.'
+                        },
+                        {
+                            title: 'Style as Button?',
+                            name: 'isButton',
+                            type: 'boolean'
+                        }
+                    ]
+                }
+            ]
+        }
+    }]
 }
