9e55bb2fc035b382025148eefcb832f0066b75f9
Gamel James <g_james@apple.com>
2021-06-22 13:15:54 -0700

    updated Faq

--- schemas/objects/sections/faq.js
+++ schemas/objects/sections/faq.js
@@ -1,26 +1,35 @@
 export default {
-  name: 'faqSection',
-  title: 'FAQS',
-  type: 'object',
-  fields: [
-    {
-      name: 'disabled',
-      title: 'Disable Section?',
-      type: 'boolean'
-    },
-    {
-      name: 'faqItems',
-      title: 'Faq',
-      type: 'array',
-      of: [{ type: 'simpleSection' }],
-      description: 'Used For questions and Answers'
+    name: 'faqSection',
+    title: 'FAQS',
+    type: 'object',
+    fields: [{
+            name: 'disabled',
+            title: 'Disable Section?',
+            type: 'boolean'
+        },
+        {
+            name: 'heading',
+            title: 'Heading',
+            type: 'string',
+            description: 'Used as the main heading of this section.',
+        }, {
+            name: 'faqItems',
+            title: 'Faq',
+            type: 'array',
+            of: [{ type: 'simpleSection' }],
+            description: 'Used For questions and Answers'
+        }
+    ],
+    preview: {
+        select: {
+            title: 'heading',
+            disabled: 'disabled'
+        },
+        prepare({ title, disabled }) {
+            return {
+                title: `${disabled ? 'DISABLED: ' : ''} ${title || 'FAQ Section'}`
+
+            }
+        }
     }
-  ],
-  preview: {
-    prepare() {
-      return {
-        title: 'FAQ Section'
-      }
-    }
-  }
 }
