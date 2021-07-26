6a5b03c40e63164eb0d09cbf7350d1becfcfe040
Gamel James <g_james@apple.com>
2021-07-26 15:02:26 -0700

    update header footer Sections

--- schemas/documents/sections/footer.js
+++ schemas/documents/sections/footer.js
@@ -1,21 +1,65 @@
 import { MdViewAgenda } from 'react-icons/md'
 
 export default {
-  name: 'sectionsFooter',
-  title: 'Footer',
-  type: 'document',
-  icon: MdViewAgenda,
-  fields: [
-    {
-      name: 'footerMenu',
-      title: 'Footer Menu',
-      type: 'reference',
-      description: 'Select the menu that will be used in the footer.',
-      to: [
+    name: 'sectionsFooter',
+    title: 'Footer',
+    type: 'document',
+    icon: MdViewAgenda,
+    fields: [{
+            name: 'addressSection',
+            title: 'Address Section',
+            type: 'object',
+            fields: [{
+                    name: 'showAddress',
+                    title: 'Show Address ',
+                    initialValue: true,
+                    type: 'boolean'
+                },
+                {
+                    name: 'showSocial',
+                    title: 'Show Social Media Icons ',
+                    initialValue: true,
+                    type: 'boolean'
+                }
+            ]
+        },
         {
-          type: 'menu'
+            name: 'footerSection',
+            title: 'Footer Section',
+            type: 'object',
+            fields: [{
+                    name: 'showFooterMenu1',
+                    title: 'Show Footer Menu 1',
+                    initialValue: true,
+                    type: 'boolean'
+                },
+
+                {
+                    name: 'footerMenu1',
+                    title: 'Footer Menu 1',
+                    type: 'reference',
+                    description: 'Select the menu that will be used in the footer 1.',
+                    to: [{
+                        type: 'menu'
+                    }]
+                },
+                {
+                    name: 'showFooterMenu2',
+                    title: 'Show Footer Menu 2',
+                    initialValue: true,
+                    type: 'boolean'
+                },
+
+                {
+                    name: 'footerMenu2',
+                    title: 'Footer Menu 2',
+                    type: 'reference',
+                    description: 'Select the menu that will be used in the footer 2.',
+                    to: [{
+                        type: 'menu'
+                    }]
+                }
+            ]
         }
-      ]
-    }
-  ]
+    ]
 }
--- schemas/documents/sections/header.js
+++ schemas/documents/sections/header.js
@@ -1,21 +1,35 @@
 import { MdViewAgenda } from 'react-icons/md'
 
 export default {
-  name: 'sectionsHeader',
-  title: 'Header',
-  type: 'document',
-  icon: MdViewAgenda,
-  fields: [
-    {
-      name: 'headerMenu',
-      title: 'Header Menu',
-      type: 'reference',
-      description: 'Select the menu that will be used in the header.',
-      to: [
+    name: 'sectionsHeader',
+    title: 'Header',
+    type: 'document',
+    icon: MdViewAgenda,
+    fields: [{
+            name: 'headerMenu',
+            title: 'Header Menu',
+            type: 'reference',
+            description: 'Select the menu that will be used in the header.',
+            to: [{
+                type: 'menu'
+            }]
+        },
         {
-          type: 'menu'
+            name: 'loginInfo',
+            title: 'Login Information',
+            type: 'object',
+            fields: [{
+                name: 'showLogin',
+                title: 'Show Login Button',
+                description: '',
+                type: 'boolean'
+            }]
+        },
+        {
+            name: 'topCtaLink',
+            title: 'Top Nav Button',
+            description: '',
+            type: 'cta'
         }
-      ]
-    }
-  ]
+    ]
 }
